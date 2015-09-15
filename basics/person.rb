36054879696d3cb1f093d63a19369f200b9cd014
Ashley Luhrs <aluhrs@gmail.com>
2015-09-14 23:10:09 -0700

    refactor

--- basics/person.rb
+++ basics/person.rb
@@ -1,47 +1,42 @@
-# First Pass. I feel like there could be a better way to do this than a class variable?
-
 # How would you define a Person class or struct so that any Person can be assigned as the parent of another Person?
 # And that the following relations between people are present?
 # bob = Person.new
 # mary = Person.new(name: "Mary", parent: bob)
 # fred = Person.new(name: "Fred", parent: bob)
+# Secondary Questions:
+# How would you create a grandchildren relation.
+# How would this work with multiple parents.
+# How would this work with N descendants.
+# person.descendants(level: 1) => # children
+# person.descendants(level: 2) => # grandchildren
+# person.descendants(levelL: 3) => # greatgrandchildren
 
 class Person
-  attr_accessor :name, :parent, :children, :siblings
-  @@all = []
-
-  def self.all
-    @@all
-  end
+  attr_accessor :name, :parent, :children#, :siblings
 
   def initialize(options={})
     @name = options[:name]
     @parent = options[:parent]
-    @@all << self
+    @children = []
+    update_children
   end
 
   # bob.children
   # [mary, fred]
-  def children
-    children = []
-    Person.all.each do |p|
-      if p.parent && p.parent == self
-        children << p.name
-      end
+  def update_children
+    if self.parent && self.name
+      parent.children << self.name
     end
-    children
   end
 
   # mary.siblings
   # [fred]
   def siblings
     siblings = []
-    Person.all.each do |p|
-      if p != self
-        if self.parent && p.parent
-          if p.parent == self.parent
-            siblings << p.name
-          end
+    if self.parent && self.parent.children
+      self.parent.children.each do |c|
+        if c != self.name
+          siblings << c
         end
       end
     end
